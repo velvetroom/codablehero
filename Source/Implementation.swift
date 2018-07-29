ba7bb8813f150651760464e3a8958dbc76538911
Zero <zero.griffin@gmail.com>
2018-07-29 12:06:06 +0200

    Create public interface

--- Source/Implementation.swift
+++ Source/Implementation.swift
@@ -1,5 +1,32 @@
 import Foundation
 
 class Implementation:CodableHero {
+    let dispatch:DispatchQueue
     
+    init() {
+        self.dispatch = DispatchQueue(label:Constants.identifier, qos:DispatchQoS.background,
+                                      attributes:DispatchQueue.Attributes.concurrent,
+                                      autoreleaseFrequency:DispatchQueue.AutoreleaseFrequency.inherit,
+                                      target:DispatchQueue.global(qos:DispatchQoS.QoSClass.background))
+    }
+    
+    func load<Model:Codable>(path:String, onCompletion:((Model) -> Void)?, onError:((Error) -> Void)?) {
+        
+    }
+    
+    func load<Model:Codable>(pathInBundle:String, onCompletion:((Model) -> Void)?, onError:((Error) -> Void)?) {
+        
+    }
+    
+    func save<Model:Codable>(model:Model, path:String, onCompletion:(() -> Void)?, onError:((Error) -> Void)?) {
+        
+    }
+    
+    func delete(path:String, onCompletion:(() -> Void)?, onError:((Error) -> Void)?) {
+        
+    }
+}
+
+private struct Constants {
+    static let identifier:String = "iturbide.codablehero"
 }
