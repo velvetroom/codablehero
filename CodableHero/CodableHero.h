33acc68ec622ac906c0ff2fee8160137ab2cc469
Zero <zero.griffin@gmail.com>
2018-07-29 11:21:31 +0200

    Initial Commit

---
+++ CodableHero/CodableHero.h
@@ -0,0 +1,19 @@
+//
+//  CodableHero.h
+//  CodableHero
+//
+//  Created by zero on 29.07.18.
+//  Copyright © 2018 iturbide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodableHero.
+FOUNDATION_EXPORT double CodableHeroVersionNumber;
+
+//! Project version string for CodableHero.
+FOUNDATION_EXPORT const unsigned char CodableHeroVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableHero/PublicHeader.h>
+
+
