4a9aa3ae1372c62b86bf0b11d743cebead4ce940
Zero <zero.griffin@gmail.com>
2018-08-03 09:01:28 +0200

    Add synchronous interface

--- Source/CodableHero.swift
+++ Source/CodableHero.swift
@@ -5,4 +5,9 @@ public protocol CodableHero {
     func load<M:Decodable>(bundle:Bundle, path:String, completion:((M) -> Void)?, error:((Error) -> Void)?)
     func save<M:Encodable>(model:M, path:String, completion:(() -> Void)?, error:((Error) -> Void)?)
     func delete(path:String, completion:(() -> Void)?, error:((Error) -> Void)?)
+    
+    func load<M:Decodable>(path:String) throws -> M
+    func load<M:Decodable>(bundle:Bundle, path:String) throws -> M
+    func save<M:Encodable>(model:M, path:String) throws
+    func delete(path:String) throws
 }
